7a94a0783381e0297eff2113070c93cb8bad0607
yangyang <1758417159@qq.com>
2024-01-22 18:57:58 +0800

    feat:新增status bar

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,65 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { log } from 'console'
-import * as vscode from 'vscode'
-
+import * as vscode from 'vscode';
+let myStatusBarItem: vscode.StatusBarItem;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+function updateStatusBarItem(): void {
+	const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+	if (n > 0) {
+		myStatusBarItem.text = `$(megaphone) ${n+1} line(s) selected`;
+		myStatusBarItem.show();
+	} else {
+		myStatusBarItem.text = 'not selected';
+		myStatusBarItem.show();
+	}
+}
+
+function getNumberOfSelectedLines(editor: vscode.TextEditor | undefined): number {
+	let lines = 0;
+	if (editor) {
+		lines = editor.selections.reduce((prev, curr) => prev + (curr.end.line - curr.start.line), 0);
+	}
+	return lines;
+}
 export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "original-resin-status" is now active!')
+    console.log('Congratulations, your extension "original-resin-status" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const date = new Date()
-    const day = date.getTime()
-    console.log(day, 'time')
-    let disposable = vscode.commands.registerCommand('original-resin-status.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage(day.toString())
-    })
+    // // The command has been defined in the package.json file
+    // // Now provide the implementation of the command with registerCommand
+    // // The commandId parameter must match the command field in package.json
+    // const date = new Date();
+    // const day = date.getTime();
+    // console.log(day, 'time');
+    // let disposable = vscode.commands.registerCommand('original-resin-status.helloWorld', () => {
+    //     // The code you place here will be executed every time your command is executed
+    //     // Display a message box to the user
+    //     vscode.window.showInformationMessage(day.toString());
+    // });
 
-    context.subscriptions.push(disposable)
+	// context.subscriptions.push(disposable);
+	// register a command that is invoked when the status bar
+	// item is selected
+	const myCommandId = 'original-resin-status.myWord';
+	context.subscriptions.push(vscode.commands.registerCommand(myCommandId, () => {
+		const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+		vscode.window.showInformationMessage(`Yeah, ${n+1} line(s) selected... Keep going!`);
+	}));
+
+	// create a new status bar item that we can now manage
+	myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 600);
+	myStatusBarItem.command = myCommandId;
+	context.subscriptions.push(myStatusBarItem);
+
+	// register some listener that make sure the status bar
+	// item always up-to-date
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem));
+
+	// update status bar item once at start
+	updateStatusBarItem();
 }
 
 // This method is called when your extension is deactivated
