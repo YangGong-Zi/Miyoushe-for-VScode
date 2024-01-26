b4590c2566658390312b5abd339a7efe0e2f7491
yangyang <1758417159@qq.com>
2024-01-26 18:36:59 +0800

    feat:打包调整

--- src/web.ts
+++ src/web.ts
@@ -1,52 +1,44 @@
-import {
-    WebviewView,
-    WebviewViewProvider,
-} from 'vscode';
-import * as vscode from "vscode";
-import {getNonce} from './getNonce';
-  export default class ReaderViewProvider implements WebviewViewProvider  {
+import { WebviewView, WebviewViewProvider } from 'vscode';
+import * as vscode from 'vscode';
+import { getNonce } from './getNonce';
+export default class ReaderViewProvider implements WebviewViewProvider {
     public static readonly viewType = 'resin.open';
-    private _view ? : WebviewView;
+    private _view?: WebviewView;
     constructor(
-      // private readonly _extensionUri: Uri,
-      protected context: vscode.ExtensionContext,
-      private readonly _extensionUri: vscode.Uri = context.extensionUri
+        // private readonly _extensionUri: Uri,
+        protected context: vscode.ExtensionContext,
+        private readonly _extensionUri: vscode.Uri = context.extensionUri
     ) {}
-    dispose(): void { }
-    public resolveWebviewView(
-      webviewView: WebviewView,
-    ) {
-      this._view = webviewView;
+    dispose(): void {}
+    public resolveWebviewView(webviewView: WebviewView) {
+        this._view = webviewView;
 
-      this._view.webview.options = {
-        enableScripts: true,
-        localResourceRoots: [this._extensionUri],
-      };
-      this._view.webview.html = this.getHtmlForWebview(this._view.webview);
+        this._view.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this._extensionUri],
+        };
+        this._view.webview.html = this.getHtmlForWebview(this._view.webview);
     }
     private getHtmlForWebview(webview: vscode.Webview) {
-      const scriptUri = webview.asWebviewUri(
-        vscode.Uri.joinPath(this._extensionUri, "dist", "static/index-Gt8FvDab.js")
-      );
-      const styleMainUri = webview.asWebviewUri(
-        vscode.Uri.joinPath(this._extensionUri, "dist", "static/index--qa-DmES.css")
-      );
-       // Use a nonce to 只允许特定脚本运行.
-    const nonce = getNonce();
-      return `
-      <!DOCTYPE html>
-		<html lang="en">
-		<head>
-		<meta charset="UTF-8">
-    <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}'; ;">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/favicon.ico">
-		<link href="${styleMainUri}" rel="stylesheet">
-    <script type="module"  nonce="${nonce}" src="${scriptUri}"></script>
-    </head>
-        <body>
-          <div id="app"></div>
-        </body>
-	</html>`;
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'dist', 'static/index-tQv3iZGc.js'));
+        const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'dist', 'static/index-HGnRy7eK.css'));
+        // Use a nonce to 只允许特定脚本运行.
+        const nonce = getNonce();
+        return `
+          <!DOCTYPE html>
+          <html lang="en">
+          <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}'; ;">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="icon" href="/favicon.ico">
+          <link href="${styleMainUri}" rel="stylesheet">
+          <script type="module"  nonce="${nonce}" src="${scriptUri}"></script>
+          </head>
+              <body>
+                <div id="app"></div>
+              </body>
+          </html>
+        `;
     }
-  }
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -16,6 +16,16 @@ export default defineConfig({
   base: './', // 空字符串或 `./`（用于嵌入形式的开发）
   build: {
     outDir: '../dist/', // 打包目录
-    assetsDir: 'static'
+    assetsDir: 'static',
+    rollupOptions: {
+      output: {
+        // 入口文件名
+        // entryFileNames: 'main.js',
+        // 块文件名
+        chunkFileNames: 'main.js',
+        // // 资源文件名 css 图片等等
+        // assetFileNames: 'static',
+      },
+    },
   },
 })
