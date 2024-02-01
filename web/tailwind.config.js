94f656c2db7bef5d6bd15bddd6e680e5318d983a
yangyang <1758417159@qq.com>
2024-02-01 17:10:18 +0800

    feat:修改文件结构

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from 'daisyui'
+export default {
+  content: ['./src/**/*.{vue,html,js}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [daisyui]
+}
